61f46875c266f552175ed28c49072bd39c3f7114
Ammar Fitwalla <50837913+ammarfitwalla@users.noreply.github.com>
2026-05-01 23:28:41 +0530

    Update data.js

--- data.js
+++ data.js
@@ -102,7 +102,7 @@ const familyData = {
     { id: 116, name: "Fatima", gender: "female", dob: "2025-01-01", deceased: false, photo: null },
     { id: 35, name: "Maria Shaikh", gender: "female", dob: "2002-01-01", deceased: false, photo: null },
     { id: 72, name: "Aaqil", gender: "male", dob: "2000-01-01", deceased: false, photo: null },
-    { id: 76, name: Abdul Kabir", gender: "male", dob: "2024-01-01", deceased: false, photo: null },
+    { id: 76, name: "Abdul Kabir", gender: "male", dob: "2024-01-01", deceased: false, photo: null },
 
     // --- Family of Ishaque Fitwalla ---
     { id: 36, name: "Ammar Fitwalla", gender: "male", dob: "1995-04-17", deceased: false, photo: null },
